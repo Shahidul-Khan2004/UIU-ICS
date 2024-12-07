d468ac7c7685ad1ce01e892206cb51bc35a931dc
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-07 23:34:44 +0600

    Added prob 4

---
+++ Assignment 1/0112430301_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+    int integer;
+    float fractional;
+    char character;
+    integer = 5;
+    fractional = 3.141593;
+    character = 'a';
+    printf("The integer value: %d\nThe floating point value: %f\nThe character value: %c", integer, fractional, character);
+}
