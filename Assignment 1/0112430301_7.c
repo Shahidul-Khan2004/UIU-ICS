ed99170b93ece7d9be4ba8510ffd7573ca4dec1a
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-07 23:57:20 +0600

    Added prob 7

---
+++ Assignment 1/0112430301_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void) {
+    int integer;
+    float fractional;
+    char character;
+    printf("Please enter an integer  : ");
+    scanf("%d", &integer);
+    printf("Please enter an float    : ");
+    scanf("%f", &fractional);
+    printf("Please enter an character: ");
+    scanf(" %c", &character);
+    printf("The integer value: %d\nThe floating point value: %f\nThe character value: %c", integer, fractional, character);
+}
