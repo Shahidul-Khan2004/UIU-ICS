90fe71f03cce2f064b3c1a42420d1cd156bd9b54
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-08 23:01:08 +0600

    Added Prob 9

---
+++ Assignment 1/0112430301_9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main (void) {
+    double i;
+    bool j;
+    i =  1.618039;
+    j = 0;
+    printf("The double value: %lf\nThe boolean value: %d", i, j);
+}
