18fdcf241c2e1e2391d0fe25aef7a94d21acbd62
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-09 16:45:18 +0600

    Added prob 12

---
+++ Assignment 1/0112430301_12.c
@@ -0,0 +1,9 @@
+//Program that will define a constant using “CONST” and print the value.
+
+#include <stdio.h>
+
+int main (void) {
+    const float pi = 3.14;
+    const float goldenRatio = 1.62;
+    printf("The value of pi: %.2f\nThe value of golden ratio: %.2f", pi, goldenRatio);
+}
