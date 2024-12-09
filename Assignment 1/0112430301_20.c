2bf18522e2d6c51fb055aa037679168b5152ff93
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-10 00:15:46 +0600

    Added prob 20

---
+++ Assignment 1/0112430301_20.c
@@ -0,0 +1,15 @@
+//Program that will increment and decrement a number X by Y. (Use += and -= operators)
+
+#include <stdio.h>
+
+int main (void) {
+    int X, Y;
+    printf("Enter the value: ");
+    scanf("%d", &X);
+    printf("Increment/Decrement the vlue by: ");
+    scanf("%d", &Y);
+
+    printf("Incremented Value: %d\n", X += Y);
+    
+    printf("Decremented Value: %d\n", X -= Y * 2);
+}
