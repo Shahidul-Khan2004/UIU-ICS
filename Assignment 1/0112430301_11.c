923e1972898e8ea12cdb4071546c46875b1910c0
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-09 16:39:56 +0600

    Added problem 11

---
+++ Assignment 1/0112430301_11.c
@@ -0,0 +1,29 @@
+/*Program that will declare a variable from each data type: unsigned int, unsigned long int,
+unsigned long long int, unsigned short int. Then it will initialize them with values and print
+them.*/ 
+
+#include <stdio.h>
+
+void commaAdder (unsigned short int num);
+
+int main (void) {
+    unsigned int a;
+    unsigned long int b;
+    unsigned long long int c;
+    unsigned short int d;
+    a = 4294967295;
+    b = 4294967295;
+    c = 18446744073709551615ULL;
+    d = 65535;
+    printf("The unsigned int value: %u\nThe unsigned long int value: %lu\nThe unsigned long long int value: %llu\n", a, b, c);
+    commaAdder(d);
+}
+
+void commaAdder (unsigned short int num) {
+    if (num < 1000) {
+        printf("he unsigned short int value: %hu", num);
+    } else {
+        commaAdder(num / 1000);
+        printf(",%03hu", num % 1000);
+    }
+}
