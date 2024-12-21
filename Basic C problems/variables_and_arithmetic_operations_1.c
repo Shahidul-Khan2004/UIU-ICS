f837fb9dc36921314f55f2334bf3df4a158363cf
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-21 20:51:48 +0600

    added a problem

---
+++ Basic C problems/variables_and_arithmetic_operations_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void) {
+    int integer = 17;
+    float floatingPoint = 3.508;
+    char character = 'W';
+    printf("Integer value = %d\nFloating point value = %.3f\nCharacter value = %c", integer, floatingPoint, character);
+}
