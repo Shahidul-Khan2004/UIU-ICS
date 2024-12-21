3ecd2fd51bab396c3c3215f513bdfd5f2210fb23
Shahidul <theshahidkhan.2004@gmail.com>
2024-12-21 20:26:33 +0600

    added a problem

---
+++ Basic C problems/printf_3.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main (void) {
+    printf("No, you cannot directly use the lorem Emmet abbreviation in a C file, because Emmet is designed primarily for markup and styling languages like HTML and CSS.\nIt does not natively support text generation in programming languages like C. However, you can use a workaround to insert Lorem Ipsum text into a C file\nYou can create a variable as a character array (char *) and print it with %%s placeholder.");
+}
