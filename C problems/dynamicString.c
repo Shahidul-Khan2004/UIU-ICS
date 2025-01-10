5d1bbc1f541b69422e560c3b8f34c05d7a967150
Shahidul <theshahidkhan.2004@gmail.com>
2025-01-10 20:50:47 +0600

    basic string

--- C problems/dynamicString.c
+++ C problems/dynamicString.c
@@ -6,12 +6,6 @@ int main (void) {
     char *str = NULL;
     char buffer[16];
     printf("Please enter a string: ");
-
-    while (fgets(buffer, sizeof(buffer), stdin))
-    {
-        size_t chunk = strlen(buffer);
-
-        char *temp = realloc(str, chunk);
-    }
-    
+    fgets(buffer, sizeof(buffer), stdin);
+    printf("%s", buffer);
 }
