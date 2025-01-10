fa809c69f12d07c103c457248f6447e478b1fedf
Shahidul <theshahidkhan.2004@gmail.com>
2025-01-10 21:47:32 +0600

    added the simple form of taking dynamic string input in C

--- C problems/dynamicString.c
+++ C problems/dynamicString.c
@@ -4,13 +4,22 @@
 
 int main (void) {
     char *str = NULL;
-    int n;
-    printf("enter the approzimate size of your string: ");
-    scanf("%d", &n);
-    getchar();
-    str = (char*) malloc((n + 1) * sizeof(char));
+    size_t n = 10;
+    size_t length = 0;
+    char *buffer = (char *) malloc ((n + 1) * sizeof(char));
     printf("Please enter a string: ");
-    fgets(str, n + 1, stdin);
+    while(fgets(buffer, n + 1, stdin)) {
+        str = realloc(str, length + n + 1);
+        strcpy(str + length, buffer);
+        length += n;
+        size_t chunk_length = strlen(buffer);
+        if (chunk_length > 0 && buffer[chunk_length - 1] == '\n')
+        {
+            break;
+        }
+    }
     str[strcspn(str, "\n")] = '\0';
     printf("%s", str);
+    free(str);
+    free(buffer);
 }
