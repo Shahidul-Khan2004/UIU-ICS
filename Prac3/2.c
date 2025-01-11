879379883188050712cd41684d3fc1b46b5ce90d
Shahidul <theshahidkhan.2004@gmail.com>
2025-01-12 00:48:12 +0600

    added a problem

---
+++ Prac3/2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void) {
+    int n;
+    printf("enter a number: ");
+    scanf("%d", &n);
+    printf("The absolute value of the number is: %d", abs(n));
+}
