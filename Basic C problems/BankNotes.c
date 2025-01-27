1ad5feb4e80e68f6368f262e12ad251775b6f190
Shahidul <theshahidkhan.2004@gmail.com>
2025-01-28 02:56:14 +0600

    fixed the code

--- Basic C problems/BankNotes.c
+++ Basic C problems/BankNotes.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 
 int main (void) {
-    int a, b, c, d, e, f, m, money;
+    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, money;
 
     scanf("%d", &money);
 
     if (money >= 100)
     {
         a = money / 100;
+    }
+
+    if (money >= 50)
+    {
         b = (money % 100) / 50;
+    }
+
+    if (money >= 20)
+    {
         c = (money % 50) / 20;
+    }
+
+    if (money >= 10)
+    {
         d = (money % 20) / 10;
-        e = (money % 10) / 5;
-        f = (money % 5);
-        printf("%d\n%d note(s) of 100\n%d note(s) of 50\n%d note(s) of 20\n%d note(s) of 10\n%d note(s) of 5\n%d note(s) of 1", money, a, b, c, d, e, f);
     }
     
+    if (money >= 5)
+    {
+        e = (money % 10) / 5;
+    }
+
+    if (money >= 1)
+    {
+        f = (money % 5);
+    }
+
+    printf("%d \n%d note(s) of 100\n%d note(s) of 50\n%d note(s) of 20\n%d note(s) of 10\n%d note(s) of 5\n%d note(s) of 1", money, a, b, c, d, e, f);
 }
